81fcb5d0d26e2740b8ba1eb82cb8f802909a4765
Mardin <mardinomer001@gmail.com>
2024-05-19 01:15:57 +0300

    I did  the schema

---
+++ server/models/shop-Item.js
@@ -0,0 +1,30 @@
+const mangoose = require("mongoose")
+const schema =new mangoose.schema({
+    name: {
+        type: String,
+        required: true,
+      },
+
+      price: {
+        type:String,
+        required: true,
+      },
+      description:{
+        type: String,
+        require: true,
+
+      },
+      
+      image: {
+        type: String,
+      },
+      availableCount: {
+        type: Number,
+        required: true,
+      },
+      genre :{
+        type: [String],
+        required: true,
+      },
+});
+module.exports = mangoose.model("shop-Item", schema);
