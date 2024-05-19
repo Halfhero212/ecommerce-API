3898714b044e5784d39a03bc65198aa573b065b9
Nooralhussein <lighthero212@gmail.com>
2024-05-19 16:24:32 +0300

    Removed extra comments and changed the project name's first letter

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
-// Using the connection string and environment variables
-const password = process.env.MONGODB_PASSWORD;  // Your password, ensure it's URL encoded if there are special characters
-const username = process.env.MONGODB_USER;       // Your MongoDB username
+const password = process.env.MONGODB_PASSWORD;  
+const username = process.env.MONGODB_USER;       
 const clusterUrl = process.env.CLUSTER_URL;
-const dbName = process.env.MONGODB_DATABASE;    // Replace with your actual database name
+const dbName = process.env.MONGODB_DATABASE;    
 
-// Ensure password is encoded in case there are special characters
 const DB_URI = `mongodb://${username}:${encodeURIComponent(password)}@${clusterUrl}/${dbName}?ssl=true&replicaSet=atlas-rygyob-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
 
 const connectToMongo = () => {
-  mongoose.connect(DB_URI)
-  .then(() => {
+  mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
     console.log("Database connected successfully");
   }).catch(err => {
     console.error("Database connection error: ", err);
