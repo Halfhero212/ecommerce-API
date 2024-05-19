7fe7f958ce3ea49328164db7a268e188cd538909
Nooralhussein <lighthero212@gmail.com>
2024-05-19 16:10:21 +0300

    modified the structure to make it work with an older version of mongo

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,24 +1,19 @@
 const mongoose = require("mongoose");
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, TEST_DB_HOST } =
-  process.env;
+// Using the connection string and environment variables
+const password = process.env.MONGODB_PASSWORD;  // Your password, ensure it's URL encoded if there are special characters
+const username = process.env.MONGODB_USER;       // Your MongoDB username
+const clusterUrl = process.env.CLUSTER_URL;
+const dbName = process.env.MONGODB_DATABASE;    // Replace with your actual database name
 
-const DB_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${
-  process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
-}:${DB_PORT}/${DB_NAME}?authSource=admin`;
-
-const url = DB_URI;
+// Ensure password is encoded in case there are special characters
+const DB_URI = `mongodb://${username}:${encodeURIComponent(password)}@${clusterUrl}/${dbName}?ssl=true&replicaSet=atlas-rygyob-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
 
 const connectToMongo = () => {
-  mongoose.connect(url, { useNewUrlParser: true });
-
-  db = mongoose.connection;
-
-  db.once("open", () => {
-    console.log("Database connected: ", url);
-  });
-
-  db.on("error", (err) => {
+  mongoose.connect(DB_URI)
+  .then(() => {
+    console.log("Database connected successfully");
+  }).catch(err => {
     console.error("Database connection error: ", err);
   });
 };
