64b901820135e61c1a191b929f5f463aeb4d9ba8
Mardin <mardinomer001@gmail.com>
2024-05-21 11:26:04 +0300

    I finshed admin

--- server/Controller/Items.js
+++ server/Controller/Items.js
@@ -23,4 +23,65 @@ const { validationResult} = require("express-validator");
         res.status(500).json({message: "Something went wrong"});
       }
     };
+    //updateItem Function
+    exports.updateItem = async(req, res) =>{
+         
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array()});
+
+        }
+           try{
+            const itemId = req.params.id;
+            const newItem = await ItemsModel.findByIdAndUpdate(itemId, req.body, {
+                new:true,
+            });
+            res.status(200).json(newItem);
+           }
+           catch (error){
+           console.log(error);
+           res.status(500).json({message: "An error occurred while updating the item on the server."})
+           }
+    };
+    //deleteItem Function
+    exports.deleteItem = async (req, res) => {
+        const itemId = req.params.id;
+      
+        try {
+          const item = await ItemsModel.findByIdAndDelete(itemId);
+          if (!item) {
+            return res.status(400).json({ message: "Item not found" });
+          }
+          res.status(200).json({ message: "Item deleted successfully" });
+        } catch (error) {
+          console.log(error);
+          res.status(500).json({ message: "An error occurred while deleting the item on the server." });
+        }
+      };
+
+
+      //searchItem Function
+      exports.searchItem = async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+      
+        try {
+          const { name } = req.query;
+      
+          const results = await ItemsModel.find({
+            name: { $regex: new RegExp(`^.*${name}.*`), $options: "i" },
+          });
+      
+          res.status(200).json(results);
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: "An error occurred while searching the item on the server" });
+        }
+      };
+      
+
+      
+
  
