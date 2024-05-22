a643a1bddd1069598f5b8a5bebdbb0cbdee77efa
Nooralhussein <lighthero212@gmail.com>
2024-05-22 23:46:49 +0300

    Added middleware to authenticate routes, added a checkAdmin function to check the admin

---
+++ server/middleware/checkAdmin.js
@@ -0,0 +1,10 @@
+
+const checkAdmin = (req, res, next) => {
+    if (req.user.role !== 'admin') {
+      return res.status(403).json({ message: 'Access denied' });
+    }
+    next();
+  };
+  
+  module.exports = checkAdmin;
+  
