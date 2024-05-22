dcde54c31b731f186252aa2a6c8742ab475ee40a
Nooralhussein <lighthero212@gmail.com>
2024-05-22 23:49:52 +0300

    Added a total price method

--- server/models/cart.js
+++ server/models/cart.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-  customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer' },
+  customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
   items: [{
-    product: { type: mongoose.Schema.Types.ObjectId, ref: 'ShopItem' },
-    quantity: { type: Number, default: 1 }
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'ShopItem', required: true },
+    quantity: { type: Number, default: 1, required: true },
+    price: { type: Number, required: true } // Adding price field to keep track of price at the time of adding to cart
   }],
-  // Add fields for total cost if necessary
+  totalCost: { type: Number, default: 0 } // Field for total cost
 });
+
+// Method to calculate the total cost
+cartSchema.methods.calculateTotalCost = function() {
+  this.totalCost = this.items.reduce((total, item) => total + (item.price * item.quantity), 0);
+};
 
 const Cart = mongoose.model('Cart', cartSchema);
 
